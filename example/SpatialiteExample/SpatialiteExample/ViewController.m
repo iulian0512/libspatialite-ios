8f330acca693c21b8e5e9d461409f4c77586c821
Taro Matsuzawa <taro@georepublic.co.jp>
2020-07-01 09:52:23 +0900

    WIP: added sqlite3 but not working

--- example/SpatialiteExample/SpatialiteExample/ViewController.m
+++ example/SpatialiteExample/SpatialiteExample/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "spatialite.h"
+#import "sqlite3.h"
 
 @interface ViewController ()
 
@@ -14,9 +16,40 @@
 
 @implementation ViewController
 
+sqlite3* db;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self dbOpen];
+}
+
+- (void)dbOpen {
+    NSLog(@"Open Database");
+    
+    NSString *db_file = @"db5.sqlite";
+    
+    NSArray *dir_paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *document_dir = dir_paths[0];
+    
+    NSString *database_path = [[NSString alloc] initWithString:[document_dir stringByAppendingPathComponent:db_file]];
+    
+    NSFileManager *file_manager = [NSFileManager defaultManager];
+    
+    if ([file_manager fileExistsAtPath:database_path] == NO) {
+        NSLog(@"File doesn't exist");
+        const char *path = [database_path UTF8String];
+        if (sqlite3_open(path, &db) == SQLITE_OK) {
+            char *error;
+            const char *sql_stmt = "select spatialite_version()";
+            sqlite3_enable_load_extension(db, 1);
+            if (sqlite3_load_extension(db, "mod_spatialite", 0, &error) == SQLITE_OK) {
+                NSLog(@"OK!");
+            } else {
+                NSLog(@"fail!!");
+            }
+        }
+    }
 }
 
 
