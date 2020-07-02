a4d829060b08b4eff6477d0c798979a9641e57bf
Taro Matsuzawa <taro@georepublic.co.jp>
2020-07-02 09:43:11 +0900

    supports ios

--- example/SpatialiteExample/SpatialiteExample/ViewController.m
+++ example/SpatialiteExample/SpatialiteExample/ViewController.m
@@ -16,18 +16,20 @@
 
 @implementation ViewController
 
-sqlite3* db;
+sqlite3 *db;
+void *cache;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+//    spatialite_init
     [self dbOpen];
 }
 
 - (void)dbOpen {
     NSLog(@"Open Database");
     
-    NSString *db_file = @"db5.sqlite";
+    NSString *db_file = @"db8.sqlite";
     
     NSArray *dir_paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *document_dir = dir_paths[0];
@@ -39,14 +41,20 @@ sqlite3* db;
     if ([file_manager fileExistsAtPath:database_path] == NO) {
         NSLog(@"File doesn't exist");
         const char *path = [database_path UTF8String];
-        if (sqlite3_open(path, &db) == SQLITE_OK) {
+        if (sqlite3_open_v2(path, &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL) == SQLITE_OK) {
             char *error;
-            const char *sql_stmt = "select spatialite_version()";
-            sqlite3_enable_load_extension(db, 1);
-            if (sqlite3_load_extension(db, "mod_spatialite", 0, &error) == SQLITE_OK) {
-                NSLog(@"OK!");
-            } else {
-                NSLog(@"fail!!");
+            char **results;
+            int rows;
+            int columns;
+            const char *sql = "select spatialite_version()";
+            //sqlite3_enable_load_extension(db, 1);
+            //if (sqlite3_load_extension(db, "mod_spatialite", 0, &error) == SQLITE_OK) {
+            cache = spatialite_alloc_connection();
+            spatialite_init_ex(db, cache, 1);
+            if (sqlite3_get_table(db, sql, &results, &rows, &columns, &error) == SQLITE_OK) {
+                for (int i = 0; i <= rows; i++) {
+                    printf("version = %s", results[(i * columns) + 0]);
+                }
             }
         }
     }
